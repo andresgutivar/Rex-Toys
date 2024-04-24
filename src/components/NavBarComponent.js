8f9a955957ec26d1a9d633edae49ee1d30aa392d
Gianfranco Labrador <labrador.gianfrancx@gmail.com>
2024-04-24 16:17:16 -0300

    CambiosNavBar.js
    realicé algunos cambios:
    - Posicion de los botones
    - Configuracion de algunos links

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -9,15 +9,24 @@ export default function NavBarComponent() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary " fixed="top" bg="primary" data-bs-theme="dark">
       <Container fluid>
-        <Navbar.Brand>Home</Navbar.Brand>
+        <Navbar.Brand>Rex Toys</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
+        <Form className="d-flex">     {/*Barra de Busqueda*/}
+            <Form.Control
+              type="search"
+              placeholder="Buscar producto"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Buscar</Button> {/*Botón Buscar*/}
+          </Form>
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Tu carrito</Nav.Link>
+            <Nav.Link style={{ marginLeft: '470px' }} href="#action1">Tu carrito</Nav.Link>
             <Nav.Link href="#action2">Ayudanos</Nav.Link>
             <NavDropdown title="Categoria" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
@@ -29,16 +38,10 @@ export default function NavBarComponent() {
                 Something else here
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="/Iniciar sesion">Inicia Sesión</Nav.Link>
+            <Nav.Link href="/Registrarse">Registrate</Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Buscar producto"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Buscar</Button>
-          </Form>
+          
         </Navbar.Collapse>
       </Container>
     </Navbar>
