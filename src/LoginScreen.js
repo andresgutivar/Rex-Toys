77907927a77c9de013109657efe172a89cefa145
AvalosAgustin <Agustin.avalos.2do7ma@gmail.com>
2024-05-02 15:10:03 -0300

    Update LoginScreen.js

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -4,14 +4,16 @@ import Button from 'react-bootstrap/Button';
 
 export default function LoginScreen() {
   return (
-    <div style={{ margin: "10px" ,display: "flex", flexDirection: "column", alignItems: "center" }}>
+    <div style={{ margin: "80px", border:"2px",padding:"20px",witdh:"100000px"  ,display: "flex", flexDirection: "column", alignItems: "center" }}>
       <Form.Label htmlFor="inputEmail" srOnly >Email</Form.Label>
       <Form.Control
+      className="emailinput"
         type="email"
         id="inputEmail"
         placeholder="Email"
         aria-describedby="emailHelpBlock"
         sm="4"
+        style={{margin:"20px"}}
       />
       <Form.Text id="emailHelpBlock" muted>
         Your email must be a valid email address.
@@ -39,4 +41,11 @@ export default function LoginScreen() {
     </div>
   );
 }
+
+
+
+
+
+
+
 
