7696c681ac7d7be5ec6cf65199caecf1baf51453
andresgutivar <andresgutierrezpro6@gmail.com>
2024-05-02 16:04:05 -0300

    Concexion a la base de datos

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import { Route, Routes } from "react-router-dom";
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 import HomeScreen from "./HomeScreen";
 import LoginScreen from "./LoginScreen";
-import React from 'react';
-import RegisterScreen from "./RegisterScreen"
+import React from "react";
+import RegisterScreen from "./RegisterScreen";
 import SurchaseScreen from "./SurchaseScreen";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyJXp0iBWPY4q-4wIj_aY1al1S7SWXkeQ",
+  authDomain: "rex-toys-cb9f2.firebaseapp.com",
+  projectId: "rex-toys-cb9f2",
+  storageBucket: "rex-toys-cb9f2.appspot.com",
+  messagingSenderId: "736618473878",
+  appId: "1:736618473878:web:c07bf2f7fa4e4e333fd5b7",
+  measurementId: "G-5FWWK43HWP",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function App() {
+  const [datos, setDatos] = useState([]);
 
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "Producto")),
+      (data) => {
+        //coneccion a la base de datos
+        let auxTempDatos = [];
+        for (let i = 0; i < data.docs.length; i++) {
+          auxTempDatos.push(data.docs[i].data());
+        }
+        setDatos(auxTempDatos);
+      }
+    );
+
+    return unsubscribe;
+  }, []);
   return (
-     <Routes>     
-        <Route exact path="/" element={<HomeScreen/>} /> 
-        <Route exact path="/Iniciar sesion" element={<LoginScreen/>} /> 
-        <Route exact path="/Registrarse" element={<RegisterScreen/>} /> 
-        <Route exact path="/Comprar" element={<SurchaseScreen/>} /> 
-        
-    </Routes> 
+    <Routes>
+      <Route path="/" element={<HomeScreen db={db} />} />
+      <Route path="/IniciarSesion" element={<LoginScreen db={db} />} />
+      <Route path="/Registrarse" element={<RegisterScreen db={db} />} />
+      <Route path="/Comprar" element={<SurchaseScreen db={db} />} />
+    </Routes>
   );
 }
 
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,21 +1,56 @@
-import React from "react";
+import { doc, setDoc } from "firebase/firestore";
+
+import { Button } from "react-bootstrap";
 import CardsItems from "./components/CardsItemsComponent";
 import ContainerHomeComponent from "./components/ContainerHomeComponent";
-import setbebe from "../src/imagenes/Set De Bebe Aprender A Comer Solo.webp";
-import gimnasio from "../src/imagenes/Gimnasio Piano Actividades Luz Sonido Bebe.webp";
 import Navbar from "./components/NavBarComponent";
+import React from "react";
+import gimnasio from "../src/imagenes/Gimnasio Piano Actividades Luz Sonido Bebe.webp";
+import setbebe from "../src/imagenes/Set De Bebe Aprender A Comer Solo.webp";
+import { v4 as uuidv4 } from "uuid";
 
-export default function HomeScreen() {
+export default function HomeScreen({ db }) {
+  function addItem() {
+    const uuid = uuidv4();
+    console.log("llegaste");
+    setDoc(doc(db, "Productos", uuid), {
+      nombre: "holas",
+    });
+  }
+
   return (
-    <div style={{ paddingTop: '60px' }}>
+    <div style={{ paddingTop: "60px" }}>
       <Navbar />
-      <ContainerHomeComponent style={{ marginTop: '10px' }}>
-        <CardsItems name={"Set de bebe"} description={"Set De Bebe Aprender A Comer Solo"} precio={"200.000$"} image={setbebe} />
-        <CardsItems name={"Gimnasio"} description={"Gimnasio Piano Actividades Luz Sonido Bebe"} precio={"200.000$"} image={gimnasio} />
-        <CardsItems name={"gol"} description={"descripcion"} precio={"20000$"} image={setbebe} />
-        <CardsItems name={"gol"} description={"descripcion"} precio={"20000$"} image={setbebe} />
-
+      <ContainerHomeComponent style={{ marginTop: "10px" }}>
+        <CardsItems
+          name={"Set de bebe"}
+          description={"Set De Bebe Aprender A Comer Solo"}
+          precio={"200.000$"}
+          image={setbebe}
+          db={db}
+        />
+        <CardsItems
+          name={"Gimnasio"}
+          description={"Gimnasio Piano Actividades Luz Sonido Bebe"}
+          precio={"200.000$"}
+          image={gimnasio}
+        />
+        <CardsItems
+          name={"gol"}
+          description={"descripcion"}
+          precio={"20000$"}
+          image={setbebe}
+        />
+        <CardsItems
+          name={"gol"}
+          description={"descripcion"}
+          precio={"20000$"}
+          image={setbebe}
+        />
       </ContainerHomeComponent>
+      <Button onClick={() => addItem()} variant="success" type="submit">
+        Agregar producto
+      </Button>
     </div>
   );
 }
